320801e3edad4f2a91436d13df869681e74e34d5
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-09-01 22:14:12 +0400

    fixed randomizer

--- blocks/room/room.js
+++ blocks/room/room.js
@@ -11,8 +11,8 @@ modules.define('room', ['i-bem__dom', 'events__channels'], function(provide, BEM
                         video: []
                     };
                     this.staff = {
-                        images: ['nerevar', 'yurich', 'beam', 'dndushkin'],
-                        video: ['eroshinev', 'vasiliy', 'vorian', 'cherninely']
+                        images: ['allinne', 'yurich', 'beam', 'dndushkin'],
+                        video: ['developer', 'vasiliy', 'vorian', 'cherninely']
                     };
 
                     var _this = this;
