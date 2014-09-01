e1789efa98d8e1fd45fb28154ba7a6479023b5b3
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-09-01 14:30:38 +0400

    added some stuff

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -12,5 +12,38 @@ module.exports = {
             url: '_index.js'
         }
     ],
-    content: 'hello'
+    content: [
+        {
+            block: 'room',
+            content: [
+                [
+                    [0,1,2,3,4,5,6,7,8,9].map(function() {
+                        return {
+                            block: 'table',
+                            mods: {
+                                size: 'big'
+                            }
+                        };
+                    }),
+                    {
+                        block: 'table',
+                        mods: {
+                            size: 'small'
+                        }
+                    },
+                ],
+                [
+                    {
+                        block: 'bean-bag'
+                    },
+                    {
+                        block: 'bean-bag'
+                    }
+                ],
+                {
+                    block: 'tv'
+                }
+            ]
+        }
+    ]
 }
