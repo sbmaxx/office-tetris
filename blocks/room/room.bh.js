baa816d8a4bd9519bf327083d3f70f87bdaf1bb1
sbmaxx <sbmaxx@gmail.com>
2014-09-02 09:09:02 +0400

    room wrapper

--- blocks/room/room.bh.js
+++ blocks/room/room.bh.js
@@ -1,13 +1,19 @@
 bh.match('room', function(ctx, json) {
     ctx.js(true);
     ctx.content([
-        ctx.content(),
+        {
+            elem: 'wrapper',
+            content: ctx.content()
+        },
         {
             elem: 'randomize'
         }
     ], true);
 });
 
+bh.match('room__wrappe', function(ctx) {
+    ctx.attr('touch-action', 'none');
+});
 bh.match('room__randomize', function(ctx) {
     ctx.tag('button');
     ctx.content('рассадить');
