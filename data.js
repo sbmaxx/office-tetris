211ba658d59820ec81fd363c4e77d11001c53ba3
sbmaxx <sbmaxx@yandex-team.ru>
2015-08-27 17:29:41 +0300

    allinne2

--- data.js
+++ data.js
@@ -1108,5 +1108,147 @@ module.exports = [
                 "name": "bean-bag"
             }
         ]
+    },
+    {
+        id: 'allinne2',
+        text: 'Вариант Алины №2',
+        data: [
+            {
+                "x": 163,
+                "y": 546,
+                "angle": 0,
+                "name": "table",
+                "login": "nerevar"
+            },
+            {
+                "x": 70,
+                "y": 215,
+                "angle": 0,
+                "name": "tree"
+            },
+            {
+                "x": 65,
+                "y": 452,
+                "angle": 180,
+                "name": "table",
+                "login": "dndushkin"
+            },
+            {
+                "x": 163,
+                "y": 452,
+                "angle": 180,
+                "name": "table",
+                "login": "invntrm"
+            },
+            {
+                "x": 65,
+                "y": 546,
+                "angle": 0,
+                "name": "table",
+                "login": "yurich"
+            },
+            {
+                "x": 320,
+                "y": 325,
+                "angle": 180,
+                "name": "table",
+                "login": "allinne"
+            },
+            {
+                "x": 166,
+                "y": 20,
+                "angle": 180,
+                "name": "table",
+                "login": "xescoder"
+            },
+            {
+                "x": 69,
+                "y": 19,
+                "angle": 180,
+                "name": "table",
+                "login": "cherninely"
+            },
+            {
+                "x": 321,
+                "y": 542,
+                "angle": 0,
+                "name": "table",
+                "login": "tenorok"
+            },
+            {
+                "x": 69,
+                "y": 115,
+                "angle": 0,
+                "name": "table",
+                "login": "developer"
+            },
+            {
+                "x": 167,
+                "y": 114,
+                "angle": 0,
+                "name": "table",
+                "login": "hjkos"
+            },
+            {
+                "x": 69,
+                "y": 360,
+                "angle": 0,
+                "name": "bean-bag"
+            },
+            {
+                "x": 298,
+                "y": 181,
+                "angle": 0,
+                "name": "bean-bag"
+            },
+            {
+                "x": 241,
+                "y": 821,
+                "angle": 180,
+                "name": "tv"
+            },
+            {
+                "x": 183,
+                "y": 854,
+                "angle": 90,
+                "name": "table",
+                "login": "sbmaxx"
+            },
+            {
+                "x": 66,
+                "y": 853,
+                "angle": 0,
+                "name": "table",
+                "login": "eroshinev"
+            },
+            {
+                "x": 279,
+                "y": 854,
+                "angle": 270,
+                "name": "table",
+                "login": "beam"
+            },
+            {
+                "x": 67,
+                "y": 759,
+                "angle": 180,
+                "name": "table",
+                "login": "vorian"
+            },
+            {
+                "x": 180,
+                "y": 237,
+                "angle": 180,
+                "name": "table",
+                "login": "developer"
+            },
+            {
+                "x": 180,
+                "y": 330,
+                "angle": 0,
+                "name": "table",
+                "login": "developer"
+            }
+        ]
     }
 ];
