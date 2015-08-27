5011553a0eed8fabff83b0288ecf297718d07560
sbmaxx <sbmaxx@yandex-team.ru>
2015-08-27 17:04:47 +0300

    corner-table

--- common.blocks/room-controls/room-controls.bh.js
+++ common.blocks/room-controls/room-controls.bh.js
@@ -16,7 +16,7 @@ module.exports = function(bh) {
         name: 'nexus',
         text: 'нексус'
     }, {
-        name: 'corner-table',
+        name: 'corner',
         text: 'угловой стол'
     }];
 
